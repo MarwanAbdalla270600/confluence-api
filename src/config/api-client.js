8957a97d109441a07489cb3e3c1fd1c4b3a0d1e9
Marwan Abdalla <marwan3m2000@gmail.com>
2024-11-07 21:30:54 +0100

    first commit

---
+++ src/config/api-client.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const baseUrl = process.env.BASEURL;
+const email = process.env.EMAIL;
+const token = process.env.TOKEN;
+
+axios.create({
+  baseURL: baseUrl,
+  headers: {
+    Authorization: `Basic ${Buffer.from(`${email}:${token}`).toString(
+      'base64'
+    )}`,
+    Accept: 'application/json',
+  },
+});
